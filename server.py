8fd440181eb946c1614660879e2f7a512829b0bf
Vít Puskajler <vit.puskajler@synergon.sk>
2024-11-14 14:41:25 +0100

    Table layout - fixed title and footer

--- server.py
+++ server.py
@@ -1432,4 +1432,4 @@ def profile():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    #app.run(debug=True) # Delete this before pushing
+    app.run(debug=True) # Delete this before pushing
